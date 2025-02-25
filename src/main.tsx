fd61d679b4cf6a44af1cf360f272c36485840bf2
Vishal Baraiya <realvixhal@gmail.com>
2025-02-25 21:17:18 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+/// main.tsx
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import MobileDetection from './MobileDetection.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MobileDetection>
+      <App />
+    </MobileDetection>
   </StrictMode>
 );
